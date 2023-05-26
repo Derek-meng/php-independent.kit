f597d489e44b9f7677c6978e5d86c05c6dfb4be0
derek <>
2023-05-26 14:53:42 +0800

    fix post

--- src/Network/Curl/Request/PostRequest.php
+++ src/Network/Curl/Request/PostRequest.php
@@ -8,23 +8,13 @@ use Independent\Kit\Network\Curl\Params\PostEncoderFactory;
 
 class PostRequest extends AbstractMethodRequest
 {
-    /**
-     * @var IPostEncoder
-     */
-    private $paramsEncoder;
-
-    protected function init(Curl $curl)
-    {
-        parent::init($curl);
-        $this->paramsEncoder = PostEncoderFactory::make($curl->getContentType());
-    }
 
     /**
      * @inheritdoc
      */
     public function encodeParams()
     {
-        $payload = $this->paramsEncoder->encode($this->curl->getOriginParams());
+        $payload = PostEncoderFactory::make($this->curl->getContentType())->encode($this->curl->getOriginParams());
         $this->curl->setActualParams($payload);
         curl_setopt($this->curl->getCurl(), CURLOPT_POSTFIELDS, $payload);
 
